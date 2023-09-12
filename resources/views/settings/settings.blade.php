19704eab22fa4f1defa44a357aca90953843d7a8
itsangelyne <itsangelyne14@gmail.com>
2023-09-13 02:04:17 +0800

    settings semi ud

--- resources/views/settings/settings.blade.php
+++ resources/views/settings/settings.blade.php
@@ -5,8 +5,29 @@
 @endsection
 
 @section('content')
-<div class="container-fluid" id="settingcon">
-    <div class="row">
-        <div class="col-md-12">
-            <h3>Basic Information</h3>
+<div class="content-container">
+    <h3>Basic Information</h3>
+    <div class="row g-3 align-items-center mb-3">
+            <div class="col-1">
+                    <label for="fullName"> Full Name</label>
+                </div>
+                <div class="col-3">
+                <input type="text" class="form-control" value="" readonly>
+            </div>
+        </div>
+        <div class="row g-3 align-items-center mb-3">
+            <div class="col-1">
+                    <label for="emailAddress">Adamson Mail</label>
+                </div>
+                <div class="col-3">
+                <input type="text" class="form-control" value="" readonly>
+            </div>
+        </div>
+        <div class="row g-3 align-items-center mb-3">
+            <div class="col-1">
+                    <label for="password">Password  </label>
+                </div>
+        </div>
+    </div>
+</div>
 @endsection
