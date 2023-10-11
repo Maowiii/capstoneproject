d7d962a57f4a630ea2223a52ee9e7bad3a0b8ac5
itsangelyne <itsangelyne14@gmail.com>
2023-10-12 01:42:45 +0800

    Update admin_self_evaluation.blade.php

--- resources/views/admin-pages/admin_self_evaluation.blade.php
+++ resources/views/admin-pages/admin_self_evaluation.blade.php
@@ -292,7 +292,7 @@
                         <td></td>
                         <td class='text-right'>Frequency:</td>
                         <td>
-                            <div class="d-flex justify-content-center gap-3">
+                            <div class="d-flex justify-content-center gap-2">
                                 <div class="col-auto">
                                     <input class="xxs-column form-control frequency-counter-5 text-center" type="text"
                                         readonly>
@@ -352,7 +352,7 @@
                         <td></td>
                         <td class='text-right'>Frequency:</td>
                         <td>
-                            <div class="d-flex justify-content-center gap-3">
+                            <div class="d-flex justify-content-center gap-2">
                                 <div class="col-auto">
                                     <input class="xxs-column form-control frequency-counter-5 text-center" type="text"
                                         readonly>
