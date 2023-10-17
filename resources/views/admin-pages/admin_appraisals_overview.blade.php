dc162ba4e8c12450cbbb4a6cadc9f288c1ca77d0
itsangelyne <itsangelyne14@gmail.com>
2023-10-17 15:32:31 +0800

    update

--- resources/views/admin-pages/admin_appraisals_overview.blade.php
+++ resources/views/admin-pages/admin_appraisals_overview.blade.php
@@ -59,7 +59,7 @@
     </div>
 
     <div class="modal fade modal" id="SummaryModal" data-bs-backdrop="static">
-        <div class="modal-dialog  modal-xl" style="width:90%">
+        <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="SummaryModal-label">Summary of Ratings</h1>
