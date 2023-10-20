1be48ab363fc8fecaaaf30163c02e90cf6ea71ee
leleindistress <kyle.martin.roperez@adamson.edu.ph>
2023-10-20 11:19:39 +0800

    testingggggggg

--- resources/views/is-pages/is_appraisal.blade.php
+++ resources/views/is-pages/is_appraisal.blade.php
@@ -2161,6 +2161,7 @@
                             $('#KRA_table_body [name$="[KRA_performance_indicator]"]').prop('disabled', false);
 
                             $('input[type="radio"]').prop('disabled', true);
+                            console.log( $('input[type="radio"]'));
                             $('#SID_table_body input[type="radio"]').prop('disabled', true).addClass('disabled');
                             $('#SR_table_body input[type="radio"]').prop('disabled', true).addClass('disabled');
                             $('#S_table_body input[type="radio"]').prop('disabled', true).addClass('disabled');
