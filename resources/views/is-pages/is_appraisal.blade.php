ae20a451d6ef4c8eff9ba4c87351167fc5f85991
leleindistress <kyle.martin.roperez@adamson.edu.ph>
2023-10-22 09:08:41 +0800

    Update is_appraisal.blade.php

--- resources/views/is-pages/is_appraisal.blade.php
+++ resources/views/is-pages/is_appraisal.blade.php
@@ -1746,13 +1746,15 @@
 
                     // Loop through the jicData and populate the table rows with data
                     data.jicData.forEach(function(jic, index) {
-                        var row = document.querySelectorAll('#jic_table_body tr')[parseInt(jic.question_order)-1];
+                        jicID = parseInt(jic.question_order);
+                        
+                        var row = document.querySelectorAll('#jic_table_body tr')[jicID];
                         console.log(row);
 
-                        var answerRadioYes = row.querySelector('input[name="feedback[' + (parseInt(jic.question_order)-1) + '][{{ $appraisalId }}][answer]"][value="1"]');
-                        var answerRadioNo = row.querySelector('input[name="feedback[' + (parseInt(jic.question_order)-1) + '][{{ $appraisalId }}][answer]"][value="0"]');
+                        var answerRadioYes = row.querySelector('input[name="feedback[' + (jicID) + '][{{ $appraisalId }}][answer]"][value="1"]');
+                        var answerRadioNo = row.querySelector('input[name="feedback[' + (jicID) + '][{{ $appraisalId }}][answer]"][value="0"]');
 
-                        var commentTextarea = row.querySelector('.textarea[name="feedback[' + (parseInt(jic.question_order)-1) + '][{{ $appraisalId }}][comments]"]');
+                        var commentTextarea = row.querySelector('.textarea[name="feedback[' + (jicID) + '][{{ $appraisalId }}][comments]"]');
                         console.log(answerRadioYes);
                         console.log(answerRadioNo);
                         console.log(commentTextarea);
