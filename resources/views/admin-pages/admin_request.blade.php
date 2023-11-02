d0b3bb3cfda6c98db6398c1f545b9f60ebbd1e88
itsangelyne <itsangelyne14@gmail.com>
2023-11-02 14:20:20 +0800

    Create admin_request.blade.php

---
+++ resources/views/admin-pages/admin_request.blade.php
@@ -0,0 +1,24 @@
+@extends('layout.master')
+
+@section('title')
+    <h1>Request Overview</h1>
+@endsection
+
+@section('content')
+<div class="content-container">
+    <div class="table-responsive">
+        <table class='table table-bordered' id="request_table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Request Note</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody id="request_body">
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
