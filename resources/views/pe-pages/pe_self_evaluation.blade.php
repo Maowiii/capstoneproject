b065cc678e41a43ce1a2807e66f08858b4fd0d88
itsangelyne <itsangelyne14@gmail.com>
2023-11-05 04:50:22 +0800

    Update pe_self_evaluation.blade.php

--- resources/views/pe-pages/pe_self_evaluation.blade.php
+++ resources/views/pe-pages/pe_self_evaluation.blade.php
@@ -885,7 +885,11 @@
                     <div id="validation-results" class="alert alert-danger" style="display: none;">
                         <ul id="validation-list"></ul>
                     </div>
-                    <label for="requestText">Request:</label>
+                    <h4>Instructions:</h3>
+                        <p class='text-justify'>This request form is designed to assist us in better serving your needs and ensuring a smooth process. 
+                        Kindly provide the details of your request and any additional notes in the fields provided. Your candid and thoughtful responses 
+                        are highly appreciated.</p>
+                    <label for="requestText"><h5>Request:</h5></label>
                     <textarea id="requestText" class="form-control" placeholder="Enter your request here..."></textarea>
                 </div>
                 <div class="modal-footer">
