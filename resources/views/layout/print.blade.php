e009762ec0593e09d261cd9e6e295879631b8a82
itsangelyne <itsangelyne14@gmail.com>
2024-02-06 15:33:07 +0800

    print

---
+++ resources/views/layout/print.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="{{ asset('master.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.2.0/dist/chart.umd.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"></script>
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
+
+    <!-- Content -->
+    <section class="content-section">
+        <div class="content-title">
+            <span class="text" id="title-heading">
+                @yield('title')
+            </span>
+        </div>
+        <hr class="content-divider">
+        <div class="content-body">
+            @yield('content')
+        </div>
+    </section>
+</html>
---
+++ resources/views/print-save/admin_dashboard_print.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.master')
+
+@section('title')
+    <h1>Overall Dashboard</h1>
+@endsection
+
+@section('content')
+    <div class="row g-3 align-items-start mb-3">
+        <h2 id="school-year-heading">School Year: -</h2>
+    </div>
+    
+@endsection
